1bd13c7ca1710e19002ecc837678206d339e7199
Fanfei Chen <frankycff@gmail.com>
2022-06-21 01:08:12 -0400

    API Deployment

---
+++ example_request.py
@@ -0,0 +1,23 @@
+import requests
+
+data_test = {
+        "age": 39,
+        "workclass": "State-gov",
+        "fnlgt": 77516,
+        "education": "Bachelors",
+        "education-num": 13,
+        "marital-status": "Never-married",
+        "occupation": "Adm-clerical",
+        "relationship": "Not-in-family",
+        "race": "White",
+        "sex": "Male",
+        "capital-gain": 2174,
+        "capital-loss": 0,
+        "hours-per-week": 40,
+        "native-country": "United-States"
+    }
+
+post_response = requests.post("http://127.0.0.1:5000/adult/", json=data_test)
+
+print(post_response.status_code)
+print(post_response.json())
